f1b5fd6e8665191c7e0f91008183c1ed4240c4e0
bartj2341 <bartefix@gmail.com>
2023-02-04 18:18:41 +0100

    FEAT: Sidebar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
-import { SearchBar } from "./index"
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
 	<Stack
@@ -15,11 +15,11 @@ const Navbar = () => (
 			justifyContent: "space-between",
 		}}
 	>
-    <Link to="/" style={{display: 'flex', alignItems: "center"}}>
-      <img src={logo} alt="logo" height={45}/>
-    </Link>
-    <SearchBar />
-  </Stack>
+		<Link to="/" style={{ display: 'flex', alignItems: "center" }}>
+			<img src={logo} alt="logo" height={45} />
+		</Link>
+		<SearchBar />
+	</Stack>
 );
 
 export default Navbar;
